89e54afee082e81b0af55c7f38108d286c7562b9
MertzAndreas <Andreasmertz11@gmail.com>
2026-06-26 15:24:59 +0200

    More packages

--- nixos/modules/packages.nix
+++ nixos/modules/packages.nix
@@ -9,6 +9,25 @@
     tree-sitter
     neovim
     tmux
+    rustup
+    rust-analyzer
+    cargo
+    lua
+    lua-language-server
+    stylua
+    nil
+    nodejs
+    vtsls
+    tailwindcss-language-server
+    clang-tools
+    typst
+    tinymist
+    python3
+    basedpyright
+    bash-language-server
+    duckdb
+    gh
+    git-filter-repo
   ];
 
   environment.pathsToLink = [ "/share/applications" "/share/xdg-desktop-portal" ];
