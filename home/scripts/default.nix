b42995c0882d7d126f20bc007b7cd8b4a3992623
MertzAndreas <Andreasmertz11@gmail.com>
2026-07-17 09:10:04 +0200

    Removed uselss scripts

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -4,13 +4,6 @@ let
     run-or-raise = pkgs.writeShellScriptBin "run-or-raise" (builtins.readFile ./bin/run-or-raise);
     kitty-cwd = pkgs.writeShellScriptBin "kitty-cwd" (builtins.readFile ./bin/kitty-cwd);
     start-keyring = pkgs.writeShellScriptBin "start-keyring" (builtins.readFile ./bin/start-keyring);
-    launch-terminal = pkgs.writeShellScriptBin "launch-terminal" (
-      builtins.readFile ./bin/launch-terminal
-    );
-    tmux-sessionizer = pkgs.writeShellScriptBin "tmux-sessionizer" (
-      builtins.readFile ./bin/tmux-sessionizer
-    );
-    nix-search = pkgs.writeShellScriptBin "nix-search" (builtins.readFile ./bin/nix-search);
   };
 in
 {
