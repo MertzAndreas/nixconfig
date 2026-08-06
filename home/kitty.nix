8325d1fdea6d71cacb17c690825d9e34f5a4c77a
MertzAndreas <Andreasmertz11@gmail.com>
2026-08-06 20:04:13 +0200

    kitty no confirm on quit

--- home/kitty.nix
+++ home/kitty.nix
@@ -10,6 +10,8 @@
       background_opacity = 0.90;
       cursor_trail = 1;
       modify_font = "cell_height 130%";
+      confirm_os_window_close = 0;
+      dynamic_background_opacity = true;
     };
     extraConfig = ''
       include dank-theme.conf
